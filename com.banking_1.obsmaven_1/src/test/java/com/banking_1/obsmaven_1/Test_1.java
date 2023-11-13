18acdd71682e47c60069601ce0b304f0783c98da
amlan2211 <amlankmishra@gmail.com>
2023-11-13 12:29:35 +0530

    "Update1"

---
+++ com.banking_1.obsmaven_1/src/test/java/com/banking_1/obsmaven_1/Test_1.java
@@ -0,0 +1,14 @@
+package com.banking_1.obsmaven_1;
+
+import org.testng.annotations.Test;
+
+public class Test_1 
+{
+	@Test
+	public void Sem()
+	{
+		System.out.println("hello java");
+		
+	}
+
+}
